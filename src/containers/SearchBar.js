72c12caeb742b5e70baf67d0fcbe93b997c0f40e
IvaTsu <ivan.tsud@gmail.com>
2018-03-14 13:36:50 +0200

    added handler for the Search Bar

--- src/containers/SearchBar.js
+++ src/containers/SearchBar.js
@@ -2,9 +2,28 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class SearchBar extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { searchTerm: '' };
+	}
+
+	_onInputChange = (event) => {
+		console.log(event.target.value);
+		this.setState({
+			searchTerm: event.target.value,
+		});
+	}
+
 	render() {
 		return (
-			<div>Search</div>
+			<form>
+				<input
+					placeholder="Enter city name"
+					value={this.state.searchTerm}
+					onChange={this._onInputChange}
+				/>
+				<button type="submit">Search</button>
+			</form>
 		);
 	}
 }
