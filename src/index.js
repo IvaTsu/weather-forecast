06c1e6d244ba114c602b6219da4cfab8ed818e01
IvaTsu <ivan.tsud@gmail.com>
2018-03-14 14:16:55 +0200

    added axious

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import ReduxPromise from 'redux-promise';
 import logger from 'redux-logger';
+
 import App from './views/app';
 import reducers from './reducers/index';
 
-const createStoreWithMiddleware = applyMiddleware(logger)(createStore);
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise, logger)(createStore);
 
 const root = document.querySelector('#app');
 ReactDOM.render(
